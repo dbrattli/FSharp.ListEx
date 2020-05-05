57230728547c69ddbf5409b6dfefbe84451c2e0a
Dag Brattli <dag.brattli@cognite.com>
2020-05-05 08:21:24 +0200

    Initial commit

---
+++ src/ListEx.fs
@@ -0,0 +1,40 @@
+﻿namespace FSharp.ListEx
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module List =
+    /// Convert F# List to C# List
+    let inline toResizeArray (xs: List<_>) =
+        ResizeArray xs
+
+    /// Alias for List.toResizeArray
+    let toCSharpList = toResizeArray
+
+    /// Convert C# List to F# List
+    let inline ofResizeArray xs =
+        List.ofSeq xs
+
+    /// Alias for List.ofResizeArray
+    let ofCSharpList = ofResizeArray
+
+[<AutoOpen>]
+module Extensions =
+    /// Convert C# List to F# List
+    type System.Collections.Generic.List<'T> with
+        member inline this.ToList () : List<'T> =
+            Seq.toList this
+
+#if !FABLE_COMPILER
+open System.Runtime.CompilerServices
+open System.Collections.Generic
+[<Extension>]
+type IEnumerableExtensions =
+    [<Extension>]
+    /// <summary>
+    /// Convert IEnumerable to F# List.
+    /// </summary>
+    /// <param name="xs">IEnumerable to convert to F# List</param>
+    /// <typeparam name="'T">Type of enumerable item.</typeparam>
+    /// <returns>F# List</returns>
+    static member inline ToFSharpList(xs: IEnumerable<'T>) = List.ofSeq xs
+#endif
---
+++ test/Tests.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.Collections.Generic
+open FSharp.ListEx
+
+[<EntryPoint>]
+let main argv =
+    // Arrange
+    let fsList = [1; 2; 3]
+
+    // Act
+    let csList = List.toResizeArray fsList // System.Collections.Generic.List
+
+    // Assert
+    assert (fsList = csList.ToFSharpList ())
+    assert (fsList = csList.ToList ())
+    assert (fsList = List.ofResizeArray csList)
+    assert (fsList = List.ofCSharpList csList)
+
+    // Act
+    let csList = List.toCSharpList fsList
+
+    // Assert
+    assert (fsList = csList.ToFSharpList ())
+    assert (fsList = csList.ToList ())
+    assert (fsList = List.ofResizeArray csList)
+    assert (fsList = List.ofCSharpList csList)
+
+    0 // return an integer exit code
